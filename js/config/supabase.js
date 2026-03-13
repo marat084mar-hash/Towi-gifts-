383a152768d21be44de43b5366129fe84d25cf71
TowiTow1 <marat084mar@gmail.com>
2026-03-14 00:11:55 +0700

    Update supabase.js

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -1,7 +1,6 @@
-supabase.from('test_table').select('*').then(result => {
-  if (result.error) {
-    console.error('Ошибка подключения к Supabase:', result.error);
-  } else {
-    console.log('Supabase клиент инициализирован успешно');
-  }
-});
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase-js.esm.js';
+
+const supabaseUrl = 'https://kkgjkqiwrppaszvkeqbe.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZ2prcWl3cnJwYXN6dmtlcWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NDI5MDksImV4cCI6MjA4NjMxODkwOX0.euUijEzkXldhHFbIuZuzePn2ppwON78Ub-MPLKm5a9Y';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
