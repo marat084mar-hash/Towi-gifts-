649a49eb49645dfda941de386a2bffb90ff8a9cf
TowiTow1 <marat084mar@gmail.com>
2026-03-14 00:27:14 +0700

    Update supabase.js

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -2,5 +2,3 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 const supabaseUrl = 'https://kkgjkqiwrppaszvkeqbe.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZ2prcWl3cnJwYXN6dmtlcWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NDI5MDksImV4cCI6MjA4NjMxODkwOX0.euUijEzkXldhHFbIuZuzePn2ppwON78Ub-MPLKm5a9Y';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
