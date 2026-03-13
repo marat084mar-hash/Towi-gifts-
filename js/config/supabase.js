5f4ed33d71daacd2a4be7813e564b8503819cc57
TowiTow1 <marat084mar@gmail.com>
2026-03-13 23:37:57 +0700

    Update supabase.js

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+// js/config/supabase.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase-js.esm.js';
 
-const supabaseUrl = 'https://kkgjkqiwrppaszvkeqbe.supabase.co'; // замените на ваш URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZ2prcWl3cnBwYXN6dmtlcWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NDI5MDksImV4cCI6MjA4NjMxODkwOX0.euUijEzkXldhHFbIuZuzePn2ppwON78Ub-MPLKm5a9Y'; // замените на ваш anon_key
+const supabaseUrl = 'https://kkgjkqiwrppaszvkeqbe.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZ2prcWl3cnBwYXN6dmtlcWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NDI5MDksImV4cCI6MjA4NjMxODkwOX0.euUijEzkXldhHFbIuZuzePn2ppwON78Ub-MPLKm5a9Y';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
