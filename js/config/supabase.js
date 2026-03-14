11b88bfb31ea42cbe37644cebbafabdd4ad692cb
TowiTow1 <marat084mar@gmail.com>
2026-03-15 02:05:23 +0700

    Update supabase.js

--- js/config/supabase.js
+++ js/config/supabase.js
@@ -1,6 +1,10 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/dist/supabase-js.esm.js';
+// Используем обычный скрипт для простоты доступа из всех файлов
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const supabaseUrl = 'https://kkgjkqiwrppaszvkeqbe.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZ2prcWl3cnJwYXN6dmtlcWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3NDI5MDksImV4cCI6MjA4NjMxODkwOX0.euUijEzkXldhHFbIuZuzePn2ppwON78Ub-MPLKm5a9Y';
 
-export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+// Создаем клиент и вешаем его на глобальное окно браузера
+window.supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log("Supabase initialized:", !!window.supabase);
